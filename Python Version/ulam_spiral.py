99310eb44fc4ba4c86f5e7eea5460e09d10b3b73
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-06-16 14:36:01 +0200

    New version

--- Python Version/ulam_spiral.py
+++ Python Version/ulam_spiral.py
@@ -1,36 +1,20 @@
-import sys
-import sympy
+from sympy import isprime
 from math import floor, sqrt
 
 spiralSize = 15  # odd number
-centralNumber = 1  # strictly positive
-
-
-def createEmptyMatrix(size):
-    m = []
-    for i in range(size):
-        m.append([])
-        for j in range(size):
-            m[i].append([])
-    return m
+centralNumber = 1
 
 
 def move(x):  # for more information see https://oeis.org/A063826
     return floor((sqrt(4 * x + 1) + 3) % 4 + 1)
 
 
-if spiralSize % 2 == 1 and centralNumber >= 1:
-    matrix = createEmptyMatrix(spiralSize)
-else:
-    sys.exit("Error")
-
+matrix = [[[] for col in range(spiralSize)] for row in range(spiralSize)]  # Create an empty matrix
 x = y = floor(spiralSize / 2)  # middle of the spiral
 
 for n in range(centralNumber, spiralSize ** 2 + centralNumber):
-    if sympy.isprime(n):
-        matrix[y][x] = n
-    else:
-        matrix[y][x] = ''
+    matrix[y][x] = n if isprime(n) else ''
+
     if move(n - centralNumber) == 1:
         x += 1  # move to the right
     elif move(n - centralNumber) == 2:
@@ -40,9 +24,8 @@ for n in range(centralNumber, spiralSize ** 2 + centralNumber):
     else:
         y += 1  # move down
 
-# Formatting
-s = [[str(e) for e in row] for row in matrix]
-lens = [max(map(len, col)) for col in zip(*s)]
-fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-table = [fmt.format(*row) for row in s]
-print('\n'.join(table))
+
+for i in range(len(matrix)):
+    for j in range(len(matrix)):
+        print(matrix[i][j], end="\t")
+    print()
