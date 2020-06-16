8db9327941d38dc2b900cbb498bfc6eb11e68abc
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-06-16 23:29:28 +0200

    Update ulam_spiral.py

--- Python Version/ulam_spiral.py
+++ Python Version/ulam_spiral.py
@@ -4,28 +4,22 @@ from math import floor, sqrt
 spiralSize = 15  # odd number
 centralNumber = 1
 
-
-def move(x):  # for more information see https://oeis.org/A063826
-    return floor((sqrt(4 * x + 1) + 3) % 4 + 1)
-
-
-matrix = [[[] for col in range(spiralSize)] for row in range(spiralSize)]  # Create an empty matrix
-x = y = floor(spiralSize / 2)  # middle of the spiral
+matrix = [[[] for col in range(spiralSize)] for row in range(spiralSize)]  # create an empty matrix
+x = y = floor(spiralSize / 2)
 
 for n in range(centralNumber, spiralSize ** 2 + centralNumber):
     matrix[y][x] = n if isprime(n) else ''
-
-    if move(n - centralNumber) == 1:
+    move = floor((sqrt(4 * (n - centralNumber) + 1) + 3) % 4 + 1)  # for more information see https://oeis.org/A063826
+    if move == 1:
         x += 1  # move to the right
-    elif move(n - centralNumber) == 2:
+    elif move == 2:
         y -= 1  # move up
-    elif move(n - centralNumber) == 3:
+    elif move == 3:
         x -= 1  # move to the left
     else:
         y += 1  # move down
 
-
-for i in range(len(matrix)):
-    for j in range(len(matrix)):
+for i in range(spiralSize):
+    for j in range(spiralSize):
         print(matrix[i][j], end="\t")
     print()
