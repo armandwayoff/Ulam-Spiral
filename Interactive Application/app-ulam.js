31d4a630006e1485ffd8ad948a3a9333afbb077d
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-06-21 13:02:12 +0200

    New version

--- Interactive Application/app-ulam.js
+++ Interactive Application/app-ulam.js
@@ -1,120 +1,56 @@
-let dimension = document.getElementById("dimension");
-let nbrCtr = document.getElementById("nbrCtr");
-let valEnt = document.getElementById("valEnt");
-let pts = document.getElementById("points");
-
-let largeurTotale = 0.3 * window.innerWidth;
-let division = dimension.value;
-let largeurCase = largeurTotale / division;
-let weightPoint = largeurCase / 2;
-
-//Coordonnées point de départ
-let startPoint = largeurTotale / 2;
-
-let xVerteur = 0;
-let yVerteur = 0;
-
-let squareLength = 0;
-let nombre = nbrCtr.value;
-let couleur = 0;
-
-let compteur = 0;
+let canvasSize = 0.3 * window.innerWidth;
 
 function setup() {
-  var canvas = createCanvas(largeurTotale, largeurTotale);
-  canvas.parent("app");
+  let canvas = createCanvas(canvasSize, canvasSize);
+  canvas.parent("canvas");
+  textAlign(CENTER, CENTER);
+  fill(0);
 }
 
 function draw() {
-  // Modification du monbre maximal de colonnes en fonction du type d'affichage des nombres
-  if(valEnt.checked) {
-    dimension.max = "31";
-    if(dimension.value > 31) {
-      dimension.value = "31";
-    }
-  } else {
-    dimension.max = "500";
-  }
+  let spiralSize = document.getElementById("spiralSize").value;
+  let centralNumber = document.getElementById("centralNumber").value;
+  let wholeNumbers = document.getElementById("wholeNumbers").checked;
+  if (spiralSize % 2 == 1 && centralNumber > 0) {
+    let x = spiralSize / 2;
+    let y = spiralSize / 2;
+    let squareSize = canvasSize / spiralSize;
+    background(255);
+    strokeWeight(squareSize / 2);
+    textSize(squareSize / 2);
+    for (let n = centralNumber; n < spiralSize ** 2 + centralNumber; n++) {
+      if (isPrime(n)) {
+        if (wholeNumbers) {
+          text(n, x * squareSize, y * squareSize);
+        } else {
+          point(x * squareSize, y * squareSize);
 
-  if(nbrCtr.value >= 1 && nbrCtr.value <= 600 && dimension.value % 2 == 1) {
-    affichageSpirale(dimension.value, nbrCtr.value, valEnt.checked);
-  } else {
-    background(255); // Ne plus afficher le spirale
+        }
+      }
+      let move = floor((sqrt(4 * (n - centralNumber) + 1) + 3) % 4 + 1);
+      if (move == 1) {
+        x++;
+      } else if (move == 2) {
+        y--;
+      } else if (move == 3) {
+        x--;
+      } else {
+        y++;
+      }
+    }
   }
 }
 
-function affichageSpirale(col, ctr, ent) { // Nombre de colonnes, Nombre central, Valeurs entières ?, Afficher les nombres premiers ?
-  //Initialisation des variables
-  division = col;
-  largeurCase = largeurTotale / division;
-  weightPoint = largeurCase / 2;
-  startPoint = largeurTotale / 2;
-  xVerteur = 0;
-  yVerteur = 0;
-  squareLength = 0;
-  nombre = ctr;
-  couleur = 0;
-  compteur = 0;
 
-  background(255);
-
-  while(squareLength < col) {
-    textSize(weightPoint);
-    textAlign(CENTER, CENTER);
-    fill(couleur, 0, 0);
-
-    for (let i = 0; i < squareLength; i++) {
-      checkPrime(nombre);
-      if(ent) {
-        noStroke();
-        fill(couleur);
-        text(nombre, startPoint + xVerteur * largeurCase, startPoint + yVerteur * largeurCase);
-      } else {
-        strokeWeight(weightPoint);
-        checkPrime(nombre);
-        stroke(couleur);
-        point(startPoint + xVerteur * largeurCase, startPoint + yVerteur * largeurCase);
-      }
-      yVerteur = yVerteur + ((-1) ** squareLength);
-      nombre++;
-      couleur = 0;
-    }
-
-    for (let i = 0; i <= squareLength; i++) {
-      checkPrime(nombre);
-      if(ent) {
-        noStroke();
-        fill(couleur);
-        text(nombre, startPoint + xVerteur * largeurCase, startPoint + yVerteur * largeurCase);
-      } else {
-        strokeWeight(weightPoint);
-        checkPrime(nombre);
-        stroke(couleur);
-        point(startPoint + xVerteur * largeurCase, startPoint + yVerteur * largeurCase);
-      }
-      xVerteur = xVerteur + ((-1) ** squareLength);
-      nombre++;
-      couleur = 0;
-    }
-
-    squareLength++;
-  }
-}
-
-function checkPrime(n) {
+function isPrime(n) {
   if (n == 1) {
-    couleur = 255;
+    return false;
   } else {
-    for (let i = 0; i ** 2 <= n; i++) {
+    for (let i = 2; i ** 2 <= n; i++) {
       if (n % i == 0) {
-        compteur++;
+        return false;
       }
     }
-    if (compteur <= 1) {
-      couleur = 0;
-    } else {
-      couleur = 255;
-    }
-    compteur = 0;
+    return true;
   }
 }
