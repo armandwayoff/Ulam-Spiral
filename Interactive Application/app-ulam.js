ac235c9a4d7f8bb5648f71f2400dfac172ca867d
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-06-21 15:48:30 +0200

    Update of the new version

--- Interactive Application/app-ulam.js
+++ Interactive Application/app-ulam.js
@@ -1,46 +1,49 @@
-let canvasSize = 0.3 * window.innerWidth;
+const SIZE = 0.3 * window.innerWidth;
 
 function setup() {
-  let canvas = createCanvas(canvasSize, canvasSize);
+  let canvas = createCanvas(SIZE, SIZE);
   canvas.parent("canvas");
   textAlign(CENTER, CENTER);
   fill(0);
 }
 
 function draw() {
+  background(255);
+
   let spiralSize = document.getElementById("spiralSize").value;
   let centralNumber = document.getElementById("centralNumber").value;
-  let wholeNumbers = document.getElementById("wholeNumbers").checked;
+
   if (spiralSize % 2 == 1 && centralNumber > 0) {
+    let wholeNumbers = document.getElementById("wholeNumbers").checked;
+    let squareSize = SIZE / spiralSize;
     let x = spiralSize / 2;
     let y = spiralSize / 2;
-    let squareSize = canvasSize / spiralSize;
-    background(255);
-    strokeWeight(squareSize / 2);
-    textSize(squareSize / 2);
+
     for (let n = centralNumber; n < spiralSize ** 2 + centralNumber; n++) {
       if (isPrime(n)) {
         if (wholeNumbers) {
+          textSize(squareSize / 2);
           text(n, x * squareSize, y * squareSize);
         } else {
+          strokeWeight(squareSize / 2);
           point(x * squareSize, y * squareSize);
-
         }
       }
-      let move = floor((sqrt(4 * (n - centralNumber) + 1) + 3) % 4 + 1);
+
+      let move = floor((sqrt(4 * (n - centralNumber) + 1) + 3) % 4 + 1); // for more information see https://oeis.org/A063826
+
       if (move == 1) {
-        x++;
+        x++; // move to the right
       } else if (move == 2) {
-        y--;
+        y--; // move up
       } else if (move == 3) {
-        x--;
+        x--; // move to the left
       } else {
-        y++;
+        y++; // move down
       }
     }
   }
 }
-
 
 function isPrime(n) {
   if (n == 1) {
